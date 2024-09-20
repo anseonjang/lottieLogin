4daa3c0744f2634ad3fd2f03cdb54075d2b16b6c
anseon jang <64743348+anseonjang@users.noreply.github.com>
2024-09-20 16:10:56 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -12,3 +12,13 @@ const aniOp = {
     autoplay: true, // 자동재생
     path: animationPath // 로티파일 경로 
 };
+
+// 로티파일 로드 및 애니메이션 표시 
+const anim = lottie.loadAnimation(aniOp);
+// .loadAnimation() 함수는,
+// 로티 제이슨 파일을 로드하고 
+// 애니메이션 렌더링시 사용하는 함수
+
+// 크기조절
+ani.style.width = '50%'; // 가로
+ani.style.height = '50%'; // 세로
